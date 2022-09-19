f5948a31df0ae179bbc00dda090e3faa9ee0f505
Gustavo Sobral <gustavosoaressobral@gmail.com>
2022-09-19 17:54:15 +0100

    Create a function that returns a random  computer choice

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Hello World!");
+let randomChoice = Math.floor((Math.random()) * 3);
+
+getComputerChoice(randomChoice);
+
+function getComputerChoice(computerChoice){
+    let string;
+    if(computerChoice === 0){
+        string = "Rock";
+        console.log(string);
+        return string;
+    }
+    else if(computerChoice === 1){
+        string = "Paper";
+        console.log(string);
+        return string;
+    }
+    else{
+        string = "Scissors";
+        console.log(string);
+        return string;
+    }
+}

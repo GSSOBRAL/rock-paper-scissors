326351e9e4d81df939e7378a887b6be03dd1b14a
Gustavo Sobral <gustavosoaressobral@gmail.com>
2022-09-19 19:19:35 +0100

    Add a counter that keeps track of who's winning and announce it at the end

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerWinCounter = 0;
+let computerWinCounter = 0;
+
 game();
 
 function getPlayerChoice(){
@@ -31,23 +34,42 @@ function game(){
         let computerSelection = getComputerChoice();
         playRound(computerSelection, playerSelection);
     }
+
+    if(playerWinCounter > computerWinCounter)
+        console.log("Congratulations! You are the Winner!")
+    else if(computerWinCounter > playerWinCounter)
+        console.log("Better luck next time! You are the Loser!")
+    else
+        console.log("It's a draw!")
 }
 
 function playRound(computerSelection, playerSelection){
     if(computerSelection === playerSelection)
         console.log("It's a draw!");
-    else if(computerSelection === "Rock" && playerSelection === "Scissors")
-        console.log("Computer wins! " + computerSelection + " beats " + playerSelection);
-    else if(computerSelection === "Paper" && playerSelection === "Rock")
-        console.log("Computer wins! " + computerSelection + " beats " + playerSelection);
-    else if(computerSelection === "Scissors" && playerSelection === "Paper")
-        console.log("Computer wins! " + computerSelection + " beats " + playerSelection);
-    else if(computerSelection === "Rock" && playerSelection === "Paper")
-        console.log("Player wins! " + playerSelection + " beats " + computerSelection);
-    else if(computerSelection === "Scissors" && playerSelection === "Rock")
-        console.log("Player wins! " + playerSelection + " beats " + computerSelection);
-    else if(computerSelection === "Paper" && playerSelection === "Scissors")
-        console.log("Player wins! " + playerSelection + " beats " + computerSelection);
+    else if(computerSelection === "Rock" && playerSelection === "Scissors"){
+        console.log("You Lose! " + computerSelection + " beats " + playerSelection);
+        computerWinCounter++;
+    }
+    else if(computerSelection === "Paper" && playerSelection === "Rock"){
+        console.log("You Lose! " + computerSelection + " beats " + playerSelection);
+        computerWinCounter++;
+    }
+    else if(computerSelection === "Scissors" && playerSelection === "Paper"){
+        console.log("You Lose! " + computerSelection + " beats " + playerSelection);
+        computerWinCounter++;
+    }
+    else if(computerSelection === "Rock" && playerSelection === "Paper"){
+        console.log("You Win! " + playerSelection + " beats " + computerSelection);
+        playerWinCounter++;
+    }
+    else if(computerSelection === "Scissors" && playerSelection === "Rock"){
+        console.log("You Win! " + playerSelection + " beats " + computerSelection);
+        playerWinCounter++;
+    }
+    else if(computerSelection === "Paper" && playerSelection === "Scissors"){
+        console.log("You Win! " + playerSelection + " beats " + computerSelection);
+        playerWinCounter++;
+    }
     else{
         console.log("Invalid Play");
         return "Invalid";
